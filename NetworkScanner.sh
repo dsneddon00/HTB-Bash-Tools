198cdf4a553c49fe47f3bc28fc8befe706645d32
Derek Sneddon <dsneddon00@gmail.com>
2022-06-15 13:26:18 -0600

    Exercise4 complete

--- NetworkScanner.sh
+++ NetworkScanner.sh
@@ -9,3 +9,14 @@ then
 else
 	domain=$1 #identifies domain as the second argument of the script in the command line
 fi # fi closes an if statement
+
+function network_range {
+	for ip in $ipaddr
+	do
+		netrange=$(whois $ip | grep "NetRange\|CIDR" | tee -a CIDR.txt)
+		cidr=$(whois $ip | grep "CIDR" | awk '{print $2}')
+		cidr_ips=$(prips $cidr)
+		echo -e "\nNetRange for $ip:"
+		echo -e "$netrange"
+	done
+}
